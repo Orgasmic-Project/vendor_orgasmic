Spark-Rom/platform_vendor_spark

Add our bootAnimations

config/common.mk
@@ -1,7 +1,7 @@
 # Allow vendor/extras to override any property by setting it first
 $(call inherit-product, vendor/spark/config/audio.mk)
 $(call inherit-product-if-exists, vendor/extras/product.mk)
-
+$(call inherit-product, vendor/spark/config/bootanimation.mk)
 PRODUCT_BRAND ?= Spark
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0

Orgasmic-Project/vendor_orgasmic

config/common.mk
@@ -1,7 +1,7 @@
 # Allow vendor/extras to override any property by setting it first
 $(call inherit-product, vendor/orgasmic/config/audio.mk)
 $(call inherit-product-if-exists, vendor/extras/product.mk)
-
+$(call inherit-product, vendor/orgasmic/config/bootanimation.mk)
 PRODUCT_BRAND ?= Orgasmic
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0