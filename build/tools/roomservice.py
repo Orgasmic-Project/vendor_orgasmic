Spark-Rom/platform_vendor_spark

Roomservice: Update Default Branch

build/tools/roomservice.py
@@ -45,7 +45,7 @@ except ImportError:
 DEBUG = False
 
 custom_local_manifest = ".repo/local_manifests/spark_manifest.xml"
-custom_default_revision = "fire"
+custom_default_revision = "spark"
 custom_dependencies = "spark.dependencies"
 org_manifest = "devices"  # leave empty if org is provided in manifest
 org_display = "Spark-Devices"  # needed for displaying

Orgasmic-Project/vendor_orgasmic

build/tools/roomservice.py
@@ -45,7 +45,7 @@ except ImportError:
 DEBUG = False
 
 custom_local_manifest = ".repo/local_manifests/orgasmic_manifest.xml"
-custom_default_revision = "fire"
+custom_default_revision = "twelve"
 custom_dependencies = "orgasmic.dependencies"
 org_manifest = "devices"  # leave empty if org is provided in manifest
 org_display = "Orgasmic-Devices"  # needed for displaying