0d21b9deba3fd375f3684a7cc6a2921149fd0796
Bicet <bicets@gmail.com>
2021-11-03 11:48:06 +0100

    Move to proton Gapps

--- config/gapps.mk
+++ config/gapps.mk
@@ -1,14 +1,14 @@
 # Gapps
 ifeq ($(WITH_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_full.mk)
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
 
 # Common Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    vendor/orgasmic/overlay-gapps/common
+# DEVICE_PACKAGE_OVERLAYS += \
+#     vendor/orgasmic/overlay-gapps/common
 
-# Exclude RRO Enforcement
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
-    vendor/orgasmic/overlay-gapps
+# # Exclude RRO Enforcement
+# PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+#     vendor/orgasmic/overlay-gapps
 
-$(call inherit-product, vendor/orgasmic/config/rro_overlays.mk)
+# $(call inherit-product, vendor/orgasmic/config/rro_overlays.mk)
 endif
